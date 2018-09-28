fc6aef485af1bb636d25edaeba25bd17b80994ff
ybcj <ybcj_lv@163.com>
2018-09-28 15:25:05 +0800

    增加form表单

--- hello.py
+++ hello.py
@@ -1,16 +1,31 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('What is you name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=name)
 
 
 @app.route('/user/<name>')
