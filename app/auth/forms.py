0f0e32e87cee0f0b1d9b1093847f8203ab4cce1a
ybcj <ybcj_lv@163.com>
2018-10-01 17:01:17 +0800

    增加修改邮箱

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -45,3 +45,13 @@ class PasswordResetForm(FlaskForm):
     password = PasswordField('New Password', validators=[DataRequired(), EqualTo('password2', message='Please must match.')])
     password2 = PasswordField('Confirm new password', validators=[DataRequired()])
     submit = SubmitField('Reset Password')
+
+
+class ChangeEmailForm(FlaskForm):
+    email = StringField('New Email', validators=[DataRequired(), Length(1, 64), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Update Email Address')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
--- app/models.py
+++ app/models.py
@@ -59,6 +59,27 @@ class User(UserMixin, db.Model):
         s = Seriallizer(current_app.config['SECRET_KEY'], expiration)
         return s.dumps({'reset': self.id}).decode('utf-8')
 
+    def generate_email_change_token(self, new_email, expiration=3600):
+        s = Seriallizer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'change_email': self.id, 'new_email': new_email}).decode('utf-8')
+
+    def change_email(self, token):
+        s = Seriallizer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        if data.get('change_email') != self.id:
+            return False
+        new_email = data.get('new_email')
+        if new_email is None:
+            return False
+        if self.query.filter_by(email=new_email).first() is not None:
+            return False
+        self.email = new_email
+        db.session.add(self)
+        return True
+
     @staticmethod
     def reset_password(token, new_password):
         s = Seriallizer(current_app.config['SECRET_KEY'])
